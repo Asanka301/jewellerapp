49e28bb237c54825be782810bfe5828556b8d179
Asanka301 <asankagamage301@gmail.com>
2024-02-02 16:29:39 +0530

    Add filer for mobile view

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,94 +2,178 @@ import React, { useState } from "react";
 import { products } from "../data/Data";
 import { Link } from "react-router-dom";
 
+import { useEffect } from "react";
+
 export default function Products() {
-  // State for storing menu items
   const [menuItems, setMenuItem] = useState(products);
 
-  // Function to filter items based on category
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Function to check if the screen is in mobile view
+  const checkMobileView = () => {
+    setIsMobile(window.innerWidth <= 768); // Adjust the breakpoint as needed
+  };
+
+  // useEffect hook to add event listener for window resize
+  useEffect(() => {
+    checkMobileView(); // Check on initial render
+
+    // Add event listener for window resize
+    window.addEventListener("resize", checkMobileView);
+
+    // Cleanup function to remove event listener
+    return () => {
+      window.removeEventListener("resize", checkMobileView);
+    };
+  }, []);
+
+  const toggleNavbar = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
+
   const filterItems = (category) => {
-    // Filter the products array based on the selected category
     const newItems = products.filter((item) => item.category === category);
-    // Update the state with the filtered items
     setMenuItem(newItems);
 
-    // If no category selected or 'all' category selected, show all items
+    // for all data show
     if (!category || category === "all") {
       setMenuItem(products);
       return;
     }
   };
 
-  // Handler for showing all items
   const handleShowAllItems = () => {
     filterItems(); // Call filterItems without a category to show all items
   };
-
   return (
     <>
-      {/* Container for the product section */}
       <div className="container-fluid fruite py-0">
         <div className="container py-2">
           <div className="text-center">
-            <div className="row g-4">
-              {/* Featured Products header */}
-              <div className="col-lg-4 text-start">
-                <h2>Featured Products</h2>
-              </div>
-              {/* Filter options */}
-              <div className="col-lg-8 text-end">
-                <ul className="nav nav-pills d-inline-flex text-center mb-5">
-                  {/* Filter options for different categories */}
-                  <li className="nav-item">
+            {/* Render ToggleWeb component in normal view */}
+            {!isMobile && (
+              <>
+                {" "}
+                <div className="row g-4">
+                  <div className="col-lg-4 text-start">
+                    <h2>Featured Products</h2>
+                  </div>
+                  <div className="col-lg-8 text-end">
+                    <ul className="nav nav-pills d-inline-flex text-center mb-6 d-lg-inline-flex">
+                      <li className="nav-item">
+                        <a
+                          className="m-2 py-2 bg-light  active"
+                          onClick={handleShowAllItems}
+                        >
+                          <h4 style={{ width: "80px" }}>All</h4>
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          className="py-2 m-2 bg-light "
+                          onClick={() => filterItems("New")}
+                        >
+                          <h4 style={{ width: "100px" }}>New</h4>
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          className="m-2 py-2 bg-light "
+                          onClick={() => filterItems("Necklaces")}
+                        >
+                          <h4 style={{ width: "130px" }}>Necklaces</h4>
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          className="m-2 py-2 bg-light"
+                          onClick={() => filterItems("Bracelets")}
+                        >
+                          <h4 style={{ width: "130px" }}>Bracelets</h4>
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          className=" m-2 py-2 bg-light "
+                          onClick={() => filterItems("Ear studs")}
+                        >
+                          <h4 style={{ width: "130px" }}>Ear Studs</h4>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </>
+            )}
+
+            {/* Render ToggleMenu component in mobile view */}
+            {isMobile && (
+              <>
+                <button
+                  className={`border  mb-2 ${isNavbarOpen ? "collapsed" : ""}`}
+                  type=""
+                  onClick={toggleNavbar}
+                  aria-label="Toggle navigation"
+                >
+                  {isNavbarOpen ? (
+                    <h4 className="mb-2" aria-hidden="false">
+                      Categories
+                    </h4>
+                  ) : (
+                    <h4 className="">Categories </h4>
+                  )}
+                </button>
+
+                <div
+                  className={`collapse navbar-collapse justify-content-center ${
+                    isNavbarOpen ? "show" : ""
+                  } bg-white`}
+                >
+                  <div className="navbar-nav">
                     <Link
-                      className="m-2 py-2 bg-light active"
+                      className="nav-item nav-link active me-3"
                       onClick={handleShowAllItems}
                     >
-                      <h4 style={{ width: "80px" }}>All</h4>
+                      <h4 style={{ width: "130px" }}>All</h4>
                     </Link>
-                  </li>
-                  <li className="nav-item">
                     <Link
-                      className="py-2 m-2 bg-light"
+                      className="nav-item nav-link active me-3"
                       onClick={() => filterItems("New")}
                     >
-                      <h4 style={{ width: "100px" }}>New</h4>
+                      <h4 style={{ width: "130px" }}>New</h4>
                     </Link>
-                  </li>
-                  <li className="nav-item">
                     <Link
-                      className="m-2 py-2 bg-light"
+                      className="nav-item nav-link active me-3"
                       onClick={() => filterItems("Necklaces")}
                     >
                       <h4 style={{ width: "130px" }}>Necklaces</h4>
                     </Link>
-                  </li>
-                  <li className="nav-item">
                     <Link
-                      className="m-2 py-2 bg-light"
+                      className="nav-item nav-link active me-3"
                       onClick={() => filterItems("Bracelets")}
                     >
                       <h4 style={{ width: "130px" }}>Bracelets</h4>
                     </Link>
-                  </li>
-                  <li className="nav-item">
                     <Link
-                      className="m-2 py-2 bg-light"
+                      className="nav-item nav-link active me-3"
                       onClick={() => filterItems("Ear studs")}
                     >
                       <h4 style={{ width: "130px" }}>Ear Studs</h4>
                     </Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
+                    <Link to="/cart" className="nav-item nav-link me-3">
+                      <span className="d-none d-md-inline"></span>
+                    </Link>
+                  </div>
+                </div>
+              </>
+            )}
+
             <div>
-              {/* Product items */}
               <div className="tab-pane fade show p-0 active">
                 <div className="row g-4">
                   <div className="col-lg-12">
                     <div className="row g-4">
-                      {/* Render each product item */}
                       {menuItems.map((val, index) => (
                         <div className="col-md-6 col-lg-4 col-xl-3" key={index}>
                           <div className="rounded position-relative fruite-item">
@@ -102,11 +186,10 @@ export default function Products() {
                               />
                             </div>
 
-                            <div className="p-2 border-secondary border-top-4 rounded-bottom">
-                              {/* Product name */}
+                            <div className="p-2  border-secondary border-top-4  rounded-bottom">
                               <h6>{val.product_name}</h6>
-                              {/* Product price */}
-                              <div className="p-2 border-secondary border-top-4 rounded-bottom">
+
+                              <div className="p-2  border-secondary border-top-4  rounded-bottom">
                                 <h4 className="">RS. {val.price}</h4>
                               </div>
                             </div>
